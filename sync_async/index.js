b65d5a54195843fc01e4c477b23103d7e9d33dd3
Ritvikjaiswal02 <ritvikjaiswal@example.com>
2025-07-03 08:15:24 +0530

    Moved project_2 out of project_1

---
+++ sync_async/index.js
@@ -0,0 +1,11 @@
+// sync
+// console.log("start 1");
+// console.log("start 2");
+// console.log("start 3");
+
+// //async
+console.log("mark");
+setTimeout(() => {
+  console.log("sam");
+}, 2000);
+console.log("jay");
